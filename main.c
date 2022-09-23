d38fc8fa1e2f90f5e33df5ff5f139f36f96b15e7
AthenaMaster <2438758332@qq.com>
2022-09-23 12:14:37 +0000

    learn devtool upgrade command

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main (int argc, char **argv)
 {
     printf ("[master branch]Hello Yocto\n");
-	
+    printf ("Learn devtool upgrade command\n");	
     return 0;
 } /* ----- End of main() ----- */
 
